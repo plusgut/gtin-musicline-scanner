1c7167ea4a7c0bea4a49b5bef63ca6041e72bba3
plusgut <github@webentwickler2-0.de>
2021-03-25 18:23:37 +0100

    [yarn] update

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
-module.exports = (env) => ({
+module.exports = (_, { mode}) => ({
 	context: path.join(__dirname, 'src'),
 	entry: [ './index.tsx' ],
-	mode: env.mode,
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'js/[name].[hash].js',
 		chunkFilename: 'js/[name].[hash].bundle.js',
-		publicPath: '/'
+		publicPath: '/',
+		clean: true
 	},
 	resolve: {
 		extensions: [ '.ts', '.tsx', '.js', '.jsx' ],
@@ -72,9 +71,6 @@ module.exports = (env) => ({
 		}
 	},
 	plugins: [
-		new CleanWebpackPlugin({
-			cleanOnceBeforeBuildPatterns: [ path.join(__dirname, 'dist') ]
-		}),
 		new HtmlWebpackPlugin({
 			title: 'gtin musicline scanner',
 			inject: 'head'
@@ -89,7 +85,7 @@ module.exports = (env) => ({
 	],
 
 	devServer:
-		env.mode === 'development'
+		mode === 'development'
 			? {
 					port: 3000,
 					clientLogLevel: 'info',
